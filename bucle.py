979f2a46941957ac8db1dad3f5bea26fae8b5c32
Murta1800 <martabestard.tecno@gmail.com>
2019-02-16 16:19:41 +0100

    initial commit

---
+++ bucle.py
@@ -0,0 +1,8 @@
+def hi(name):
+    print('uola ' + name + '!')
+nines = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'You']
+for name in nines:
+    hi(name)
+    print('Next girl')
+for i in range(1, 6):
+    print(i)
---
+++ prova.py
@@ -0,0 +1,12 @@
+def saludo():
+    name = 'Marta'
+    if name == 'Ola':
+        print('Hey Ola!')
+    elif name == 'Sonja':
+        print('Hey Sonja!')
+    elif name == 'Marta':
+        print('hey Marta') #no sé el nom??
+    else:
+        print('Hey anonymous!')
+
+saludo()
